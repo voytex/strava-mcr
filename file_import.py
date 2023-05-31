1577873a10f4c5f563296b20523602f976b7c873
Vojtěch Lukáš <vlukas@hotmail.cz>
2023-05-31 14:19:16 +0200

    start over

--- file_import.py
+++
@@ -1,7 +0,0 @@
-import csv
-# static
-
-def import_csv(path):
-    with open(path) as f:
-        file = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
-    return file
